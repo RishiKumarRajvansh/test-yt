96634895b5d2c65b32c1d08ade159064754507e3
Rishi <rishikumarrajvansh@gmail.com>
2025-04-02 17:42:10 +0530

    minor

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def download_video():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
